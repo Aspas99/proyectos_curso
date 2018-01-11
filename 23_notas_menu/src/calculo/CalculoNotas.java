8ab8105caa7d8e8a614aaa9523fafe5eab602921
manana <manana@Core11-15>
2018-01-11 10:27:25 +0100

    Versión 1 de programa de lectura de notas

---
+++ 23_notas_menu/src/calculo/CalculoNotas.java
@@ -0,0 +1,43 @@
+package calculo;
+
+import java.util.ArrayList;
+
+public class CalculoNotas {
+
+	public  ArrayList<Double> notas=new ArrayList<>();
+	
+	public  void muestraNotas() {
+		
+		if (notas.size()>0) {
+			for (Double n:notas) {
+				System.out.print(n + "\t");
+			}
+		}
+		
+		
+	}
+	
+	public  Double media() {
+		Double suma=0.0; 
+		for (Double n:notas) {
+			suma+=n.doubleValue();
+			
+		}
+		return (Double)suma/notas.size();
+	}
+	
+	public int aprobados() {
+		int aprobados=0;
+		for (Double n:notas) {
+			if (n.doubleValue()>=5.0) aprobados++;
+		}
+		
+		return aprobados;
+	}
+	public void anadirNota(Double nota) {
+		if (nota>=0 && nota<=10)
+			notas.add(nota);
+	}
+	
+	
+}
---
+++ 23_notas_menu/src/principal/menu.java
@@ -0,0 +1,73 @@
+package principal;
+
+import java.util.Scanner;
+
+import calculo.CalculoNotas;
+
+public class menu {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		CalculoNotas n=new CalculoNotas();
+		String valor;
+		
+		String opcion;
+		do {
+		    
+		
+			System.out.println("1.- Añadir nota");
+			System.out.println("2.- Ver aprobados");
+			System.out.println("3.- Nota media ");
+			System.out.println("4.- Mostrar notas");
+			System.out.println("5.- Salir");
+			
+			System.out.print("Introduzca la opción de menu: ");
+			opcion=sc.nextLine();
+			
+			int t=Integer.valueOf(opcion);
+			switch(t) {
+				case 1:
+						System.out.println("Introduzca una nota: ");
+						valor=sc.nextLine();
+						Double v=Double.valueOf(valor);
+						if (v.doubleValue()>=0.0) {
+							
+							n.anadirNota(v);
+						}else {
+						System.out.println("¡la nota no puede ser negativa!");
+						}
+						
+				break;
+				case 2:
+						if (n.notas.size()==0) {
+							System.out.println("No hay notas por mostrar");
+						}else {
+							System.out.println("Hay :" + n.aprobados() +" aprobados");
+						}
+						
+					break;	
+				case 3:
+						System.out.println("La media es: " + n.media());
+						break;
+				case 4:
+						if (n.notas.size()==0) {
+							System.out.println("No hay notas por mostrar");
+						}else {
+							n.muestraNotas();
+						}
+						break;
+				case 5:{
+					System.out.println("Hasta la próxima!");
+					break;
+				}default:
+					System.out.println("Opción no disponible!");
+		     }
+			
+		
+			
+		}while(!opcion.equals("5"));
+
+	}
+
+}
