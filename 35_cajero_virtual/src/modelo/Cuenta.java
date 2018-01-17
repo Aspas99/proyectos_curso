66cc9bda7564558d7e5a90f433e223e26377e4c3
manana <manana@Core11-15>
2018-01-17 12:16:10 +0100

    Ejercicio 35 :Enunciado a desarrollar para herencia y practica en casa

---
+++ 35_cajero_virtual/src/modelo/Cuenta.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Cuenta {
+	// La clase dispone de los atributos
+	private double saldo;
+	
+	public Cuenta(double saldo) {
+		
+			this.saldo=saldo;
+	}
+	
+	public void ingresar(double cant) {
+		saldo+=cant;
+	}
+	public void extraer(double cant) {
+		saldo=-cant;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	
+
+}
---
+++ 35_cajero_virtual/src/presentacion/Cajero.java
@@ -0,0 +1,23 @@
+package presentacion;
+//El programa deberá
+//al iniciar el programa ,se solicita al usuario el saldo inicial y el límite de la cuenta(maxima cantidad a extraer durante el proceso)
+//La clase cuenta es una clase base que servirá como superclase a las que se necesiten
+//TRas el inicio se muestra el menú
+//1.-Ingresar
+//2.-Extraer
+//3.-Movimientos y saldo
+//4.-Salir
+//1.-Se solicita la cantidad a ingrear y se procede al ingreso y vuelve al menu
+//2.-Se solicita la cantidad a extraer, si es inferior o igual al límite se realiza la extracción.Si no,no se hace 
+//3.-Muestra los movimientos realizados en la cuenta y el saldo final. Cada movimiento se caracteriza por un tipo (ingreso o extraccion) 
+//   y una cantidad
+// La idea es aplicar la herencia para crear una subclase de cuenta para usarla en este ejercicio
+
+public class Cajero {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
