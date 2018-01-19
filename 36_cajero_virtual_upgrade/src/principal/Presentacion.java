9df434585659a174b672d95e7909fca4b942a398
manana <manana@Core11-15>
2018-01-19 13:58:37 +0100

    Primer version Listado Ventas

---
+++ 36_cajero_virtual_upgrade/src/principal/Presentacion.java
@@ -0,0 +1,17 @@
+package principal;
+/*
+ Se inicia el programa solicitando el numero de cuenta que se incluye en la BBDD bancabd de prácticas y movimientos
+ 
+ Se accede a un menu tal como este
+ 1.- Ingresar
+ 2.- Extraer
+ 3.- Transferencia
+ 4.- Ultimos movimientos
+ 5.- Salir
+ Transferencia : pide la cuenta destino y la cantidad .La de origen es la introducida al inicio (extraer en una - ingreso en otra) 
+ y reflejarla en la tabla movimientos
+ Ultimos movimientos solo saca los ultimos 5 movimientos de la cuenta
+  */
+public class Presentacion {
+
+}
