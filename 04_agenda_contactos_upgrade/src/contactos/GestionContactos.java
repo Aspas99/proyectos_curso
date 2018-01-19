1b8a1670cb507d7dd93c23c321b1c0170e3551e7
manana <manana@Core11-15>
2018-01-19 11:23:52 +0100

    Version de Agenda contactos con BBDD y el uso de ArrayList, List e implementacion de metodos abstractos con expresiones lambda

---
+++ 04_agenda_contactos_upgrade/src/contactos/GestionContactos.java
@@ -0,0 +1,132 @@
+package contactos;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.Contacto;
+
+
+
+public class GestionContactos  {
+
+	private String nombre;
+	private String email;
+	private int telefono;
+	
+	
+	Statement st;
+	String sql;
+	
+	
+		
+	
+	public void alta(String nombre,String email,int telefono)  {
+		try (Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "root"))
+ 			{
+		/*	
+			this.st=cn.createStatement();
+			sql="Insert into contactos(nombre,email,telefono) values('" +  nombre + "','" + email + "'," +telefono+")";
+			st.execute(sql);
+		*/
+			//consultas preparadas
+			String sql="Insert into contactos(nombre,email,telefono) values(?,?,?)";
+			PreparedStatement ps = cn.prepareStatement(sql);
+			ps.setString(1,nombre);
+			ps.setString(2,email);
+			ps.setInt(3, telefono);
+			ps.execute();//en este caso no se pasa como parametro la query en el string
+			
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}/*finally {
+			try {
+				if (cn!=null){ //Para que no genera un NullPointerException 
+				cn.close();
+				}
+			} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	}
+	
+	
+	public void eliminar(String email) {
+		try (Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "root");) {
+			
+			this.st=cn.createStatement();
+			
+			sql="delete from contactos where email='" + email +"'";
+			st.execute(sql);
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}/*finally {
+			try {
+				if (cn!=null){ //Para que no genera un NullPointerException 
+				cn.close();
+				}
+			} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
+		
+	}
+	
+	@SuppressWarnings("finally")
+	public Contacto buscar(String email) {
+		Contacto contacto=null;
+		try (Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "root");) {
+			
+			this.st=cn.createStatement();
+			sql="select * from contactos where email='" + email +"'";
+			ResultSet rs=st.executeQuery(sql);
+			if (rs.next()) {
+				contacto=new Contacto(rs.getString("nombre"),rs.getString("email"),rs.getInt("telefono"));
+				
+			}
+						
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+			return contacto;
+	}
+	
+	
+	public List<Contacto> recuperarTodos() {
+		//DEvolvemos un interfaz List para "desacoplar" capas 
+	  //ArrayList implementa la interfaz, por lo que se puede devolver para "aislar" el tipo de estructura que se devuelve o para
+		//que si se quiere cambiar a otra estructura sea más facil cambiarlo en el codigo de llamada
+		
+		ArrayList<Contacto> contactos = new ArrayList<>();
+		try (Connection cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "root");) {
+			
+			this.st=cn.createStatement();
+			
+			sql="select * from contactos";
+			ResultSet rs=st.executeQuery(sql);
+			//Statement st2=cn.createStatement();
+
+			//st2.execute("insert into contactos(nombre, email, telefono) values('lolo','lolo@ff.es',8998)");//No da error, pero el result set no lo incluye			
+				while (rs.next()) {
+					contactos.add(new Contacto(rs.getString("nombre"),rs.getString("email"),rs.getInt("telefono")));
+				}
+			
+			
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		return contactos;
+	}
+	
+	
+	
+}
---
+++ 04_agenda_contactos_upgrade/src/principal/Presentacion.java
@@ -0,0 +1,96 @@
+package principal;
+
+import java.util.List;
+import java.util.Scanner;
+
+import beans.Contacto;
+import contactos.GestionContactos;
+
+public class Presentacion {
+	//el ejercicio hará lo siguiente
+	//1.-Agregar contactos ->pedirá nombre, mail y telefono
+	//2.-Recuperar contactos ->pedirá el email, elimnando todos los que correspndan
+	//3.-Salir
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		GestionContactos gcontactos=new GestionContactos();
+		String opcion;
+		int t=0;
+		System.out.println("1.-Agregar contactos");
+		System.out.println("2.-Eliminar contactos");
+		System.out.println("3.-Buscar contacto");
+		System.out.println("4.-Mostrar todos");
+		System.out.println("5.-Salir");
+		System.out.println("Introduzca la opcion:");
+		opcion=sc.nextLine().toString();
+		t=Integer.valueOf(opcion);
+		
+		switch(t) {
+			case 1:{
+				leerContacto();
+				break;
+			}case 2:{
+				eliminarContacto();
+				break;
+			}
+			case 3:{
+				buscarContacto();
+				break;
+			}
+			case 4:{
+				List<Contacto> todos = gcontactos.recuperarTodos();//Implementa el método accept del forEach (accept(T t)  		
+				todos.forEach(c->System.out.println(c.getNombre() + "-" +c.getEmail() + "-"+ c.getTelefono()));//Expresion lambda c->codigo
+				break;
+			}default: System.out.println("Opcion no disponible");
+		}
+			
+
+	}
+	
+	static void leerContacto() {
+		Scanner sc= new Scanner(System.in);
+		String nombre,email;
+		
+		GestionContactos agenda=new GestionContactos();
+		System.out.println("Introduce el nombre:");
+		nombre=sc.nextLine();
+		System.out.println("Introduce el email:");
+		email=sc.nextLine();
+		System.out.println("Introduce el telefono:");
+		String tel=sc.nextLine();
+		agenda.alta(nombre, email, Integer.valueOf(tel));
+	}
+	
+	static void buscarContacto() {
+	
+		Scanner sc= new Scanner(System.in);
+		String email;
+		
+		GestionContactos agenda=new GestionContactos();
+		
+		System.out.println("Introduce el email:");
+		email=sc.nextLine();
+		Contacto contacto=agenda.buscar(email);
+		if (contacto==null) {
+			System.out.println("No existe el contacto");			
+		}else
+			System.out.println("Nombre:" + contacto.getNombre() + " Email:" + contacto.getEmail() + " Telefono:" + contacto.getTelefono());
+	}
+	
+
+	static void eliminarContacto() {
+		Scanner sc= new Scanner(System.in);
+		String email;
+		
+		GestionContactos agenda=new GestionContactos();
+		
+		System.out.println("Introduce el email:");
+		email=sc.nextLine();
+		
+		agenda.eliminar(email);
+	}
+	
+	
+		
+		
+}
