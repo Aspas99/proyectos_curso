4ac1343ce40f669a560f8ff89fb3c851aae74f7e
manana <manana@Core11-15>
2018-01-24 13:56:17 +0100

    Primera version ejercicio Cajero + Beans + BBDD + fichero configuracion

---
+++ 15_cajero_bbdd_upgrade_ficheros/src/principal.java
@@ -0,0 +1,7 @@
+
+/*La informacion de cadena de conexion , user y clave de la BBDD esté en un fichero aparte 
+ * que lo lea y vuelque la info a las clases de negocio
+ * */
+public class principal {
+
+}
