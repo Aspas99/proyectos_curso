dc36e1dc09667c86445f92a5ffad00c944467dcc
manana <manana@Core11-15>
2018-01-24 13:54:42 +0100

    Primera version practica parte meteorologico

---
+++ 14_parte_meteorologico/src/principal/Presentacion.java
@@ -0,0 +1,16 @@
+package principal;
+
+/*
+ * Te pide los datos para registrar los datos de la ciudad:nombre y temperatura de la ciudad y lo salva a fichero.
+ * en bucle para registrar "n" ciudades y su temeperatura
+ * 1.-Datos ciudad
+ * 2.-Temperatura media
+ * 3.-Ciudad mas calurosa
+ * 4.-Mostrar todas (nombre + temperatura)
+ * 5.-Salir
+ * Capa de negocio beans + capa intermedia 
+ * 
+ * */
+public class Presentacion {
+
+}
